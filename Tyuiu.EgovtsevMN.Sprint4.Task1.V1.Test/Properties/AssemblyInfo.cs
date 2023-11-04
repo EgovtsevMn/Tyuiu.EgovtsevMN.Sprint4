83706ddc1e4dbea5969695eb7e832072faa2ca85
TyuiuEgovtsevMN@outlook.com <TyuiuEgovtsevMN@outlook.com>
2023-11-04 13:07:10 +0500

    Task1

---
+++ Tyuiu.EgovtsevMN.Sprint4.Task1.V1.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.EgovtsevMN.Sprint4.Task1.V1.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.EgovtsevMN.Sprint4.Task1.V1.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("03efc1e7-da4c-486b-8d8d-807f1ec846a5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.EgovtsevMN.Sprint4.Task1.V1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.EgovtsevMN.Sprint4.Task1.V1.Lib;
+
+namespace Tyuiu.EgovtsevMN.Sprint4.Task1.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+            Console.Title = "Спринт #4 | Выполнил: Еговцев М. Н. | АСОиУб-23-1";
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* Спринт #4                                                                            *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                                    *");
+            Console.WriteLine("* Задание #1                                                                           *");
+            Console.WriteLine("* Вариант #1                                                                           *");
+            Console.WriteLine("* Выполнил: Еговцев Максим Николаевич | АСОиУб-23-1                                    *");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                             *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный значениями           *");
+            Console.WriteLine("* с клавиатуры в диапазоне от 0 до 9 подсчитать сумму нечетных элементов массива.      *");
+            Console.WriteLine("* С клавиатуры: 8, 2, 7, 5, 0, 7, 4, 7, 5, 7                                           *");
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                     *");
+            Console.WriteLine("****************************************************************************************");
+
+
+
+
+            int[] arr = new int[10];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("Введите " + i + " элемент массива: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("****************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                           *");
+            Console.WriteLine("****************************************************************************************");
+
+            Console.WriteLine("Сумма четных элементов массива = " + ds.Calculate(arr));
+
+            Console.ReadLine();
+        }
+    }
+}
