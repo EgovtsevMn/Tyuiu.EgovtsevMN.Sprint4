5093d5ffca827c5c49708cb5b26ebf5550145a69
TyuiuEgovtsevMN@outlook.com <TyuiuEgovtsevMN@outlook.com>
2023-11-12 12:49:40 +0500

    Task4

---
+++ Tyuiu.EgovtsevMN.Sprint4.Task4.V16.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.EgovtsevMN.Sprint4.Task4.V16.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.EgovtsevMN.Sprint4.Task4.V16.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e194a56f-8ec8-4016-be2f-b9595877dd6f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
